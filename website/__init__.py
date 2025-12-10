421d7ba6336865a755223108c15f22b95303eadf
Juno Phan <phanngankhanhpct@gmail.com>
2025-12-10 16:34:02 -0600

    fix database config

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'gsgsgggdsfsdsd'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////tmp/{DB_NAME}'
     db.init_app(app)
 
     
